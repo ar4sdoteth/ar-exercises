91d7827999911ccb5b6b2c155d29df32300f5680
Ashley <ashley.barr@ar4s.com>
2021-02-03 15:26:56 -0700

    add excercise 4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,13 +5,10 @@ puts "----------"
 
 # Your code goes below here ...
 
-store = Store.create([{
-  name: 'Burnaby', annual_revenue: 3000, mens_apparel: true, womens_apparel: true
-  },
-  { name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true
-  },
-  { name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false
-  }
-])
 
-puts store.count
+
+Store.create(name: 'Burnaby', annual_revenue: 3000, mens_apparel: true, womens_apparel: true)
+Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
