68a50ce92f1c3c8ddc62a962eaaf6ae5653c7423
Ashley <ashley.barr@ar4s.com>
2021-02-03 17:07:24 -0700

    add exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store4 = Store.find_by(id: 4)
+@store5 = Store.find_by(id: 5)
+@store6 = Store.find_by(id: 6)
+
+# puts "store 6 #{@store6.name}"
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Ashley", last_name: "Barr", hourly_rate: 5)
+@store4.employees.create(first_name: "Brock", last_name: "Lesner", hourly_rate: 80)
+@store5.employees.create(first_name: "Steve", last_name: "Wozniak", hourly_rate: 5)
+@store6.employees.create(first_name: "Jeff", last_name: "Bezos", hourly_rate: 60000)
+@store1.employees.create(first_name: "Daniel", last_name: "Taylor", hourly_rate: 25)
